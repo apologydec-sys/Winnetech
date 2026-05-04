8f3dd895688168b580501f24033a5b331e4c9569
WTI System <wti@winnitech.edu.gh>
2026-05-04 00:47:22 -0800

    Fix routing.py corruption and asgi.py import order

--- winnitech/asgi.py
+++ winnitech/asgi.py
@@ -1,10 +1,11 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'winnitech.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import core.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'winnitech.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
